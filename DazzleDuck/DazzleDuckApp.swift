bbd4c1c44a0c22419e7b0c4608e4f42340857ded
Kat <katherinekelli.creates@gmail.com>
2025-11-17 10:39:31 -0600

    Initial Commit

---
+++ DazzleDuck/DazzleDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  DazzleDuckApp.swift
+//  DazzleDuck
+//
+//  Created by Katherine Pizzirusso on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct DazzleDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
